d67e8624e87cff475a847388a5a0e34a537e877f
haurespro <haurespro@gmail.com>
2025-07-22 06:17:23 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@
 # تحديث مستودعات الحزم وتثبيت مكتبات التطوير الأساسية
 apt-get update -y
 apt-get install -y libpq-dev libssl-dev libffi-dev build-essential python3-dev
+
+# ثم قم بتشغيل أمر التثبيت الطبيعي لـ pip
+pip install -r requirements.txt
