f07870377e493a4dc8162495b42d31fa2f8293cc
haurespro <haurespro@gmail.com>
2025-07-24 03:41:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import logging
 import psycopg2
 from aiogram import Bot, Dispatcher, types
 from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.dispatcher import FSMContext
+from aiogram.fsm.context import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.utils.executor import start_webhook, start_polling # سنستخدم start_webhook
 
